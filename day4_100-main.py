c2e7316678552dbeaec43bdf3a1a36b8cd682a2d
DorcasOpatola <preciousopal.od@gmail.com>
2025-09-04 22:03:41 +0100

    what is your superpower?

---
+++ day4_100-main.py
@@ -0,0 +1,14 @@
+print("===EPIC ADVENTURE===")
+print()
+
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+print()
+
+name = input("What is your name?: ")
+superpower = input("What is your superpower?: ")
+location = input("Where do you live?: ")
+enemy = input("What is your worst enemy name?: ")
+favFood = input("What is your favorite food?: ")
+print()
+
+print("Hello", name, "Your ability to", superpower, "will make sure you never have to look at", enemy, "again. Go eat your", favFood, "\033[36m", "as you walk down the streets", "\033[0m", "of", location, "and use", superpower, "for good and not evil!")
