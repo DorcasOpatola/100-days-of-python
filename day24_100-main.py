25c8034b7259f3dfc5595bbf155fe801a05679c6
DorcasOpatola <preciousopal.od@gmail.com>
2025-09-19 11:34:00 +0100

    day 24

--- day24_100-main.py
+++ day24_100-main.py
@@ -1,4 +1,20 @@
 '''
+
+
+def pizza_order(topping1, topping2):
+  if topping1 == "pepperoni":
+    print(topping1, "is a great choice")
+  else:
+    print(topping1, "is kinda lame on pizza")
+  print(topping2, "sounds delicious, much better than", topping1)
+
+topping1 =  input("Name a pizza topping > ")
+topping2 = input("Name a second pizza topping > ")
+
+pizza_order(topping1, topping2)
+'''
+
+'''
 THIS CODE PRINTS OUT THE BIGGER OF TWO NUMBERS
 
 def biggerNumber(number1, number2):
