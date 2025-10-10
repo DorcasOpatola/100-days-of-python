babe9a38dd1e01d9e2b045c36eea45cd84b56629
DorcasOpatola <preciousopal.od@gmail.com>
2025-10-10 12:09:58 +0100

    module 2

---
+++ Python-For-Everybody_course/hello.py
@@ -0,0 +1,24 @@
+print("Hello, world!")
+
+
+'''
+pwd - print working directory
+mkdir - make directory
+ls - list files
+ls -l - list all files with details
+ls -a - list all files including hidden files
+ls -la - list all files with details including hidden files
+ls -lh - list all files with human readable file sizes
+ls -lha - list all files with details including hidden files with human readable file sizes
+ls -R - list all files recursively
+ls -t - list all files sorted by modification time
+ls -S - list all files sorted by file size
+ls -r - list all files in reverse order
+ls -ltr - list all files sorted by modification time in reverse orde
+cd - change directory
+touch - create a file
+cat - concatenate and display file content
+nano - text editor
+python3 hello.py - run the python file named 'hello.py'
+
+'''
