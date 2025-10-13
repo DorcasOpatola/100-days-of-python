bb55394590962e95a606feea1261b8d40f9cb61d
DorcasOpatola <preciousopal.od@gmail.com>
2025-10-13 16:02:21 +0100

    test code

--- Python-For-Everybody_course/welcome.py
+++ Python-For-Everybody_course/welcome.py
@@ -1,2 +1,15 @@
 
 print("Welcome to Python for Everybody!")
+
+x = int(input("Assign a value to x: "))
+print (x)
+
+xx = x * 2
+print (xx)
+
+if x < 10:
+    print("smaller")
+if x > 21:
+    print("bigger")
+else:
+    print("finis")
