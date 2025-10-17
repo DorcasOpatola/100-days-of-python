aba3f4467e8e10d3a05ef5be1d404486241dc984
DorcasOpatola <preciousopal.od@gmail.com>
2025-10-17 09:50:00 +0100

    functions in python

---
+++ Data-Science-In-Python/functions-module1.py
@@ -0,0 +1,26 @@
+def addNumbers (x, y, z = None, flag = False):
+    if (flag):
+        print("Flag is True!")
+    if z == None:
+        return x + y
+    else:
+        return x + y + z
+ 
+def num3 (q, w, e=None):
+    if e is None:
+        return q + w
+    else:
+        return q + w + e 
+
+num3(3, 55)
+num3(3, 55, 21)
+addNumbers(23, 9, 18)
+
+see = input("Enter three numbers: ")
+parts = see.split()
+if len(parts) == 2:
+    print(addNumbers(int(parts[0]), int(parts[1])))
+elif len(parts) == 3:
+    print(addNumbers(int(parts[0]), int(parts[1]), int(parts[2])))
+else:
+    print("Too many numbers provided")
