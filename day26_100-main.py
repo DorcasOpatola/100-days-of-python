e45bc5846607bf5d89ce04fd658b44faa3ae90aa
DorcasOpatola <preciousopal.od@gmail.com>
2025-10-25 07:43:39 +0100

    os and time libraries

---
+++ day26_100-main.py
@@ -0,0 +1,23 @@
+
+
+
+'''
+DAY 26 --- USE OF PYTHON LIBRARIES 'os' AND 'time'.
+
+import os   # The os library allows us to interact with the operating system.
+import time     # The time library allows us to work with time-related tasks, like pausing the program for a certain duration.
+
+for i in range(1,1000):
+  print(i)
+  time.sleep(0.01)
+  os.system("clear")
+
+print("Welcome")
+print("to")
+print("Replit")
+
+time.sleep(1)
+os.system("clear")
+
+username = input("Username: ")
+'''
