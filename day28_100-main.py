5d76c436b640bf352ffda0061cb4177ca7ac881e
DorcasOpatola <preciousopal.od@gmail.com>
2025-11-26 16:48:19 +0100

    implemented absolute difference

--- day28_100-main.py
+++ day28_100-main.py
@@ -3,6 +3,7 @@ import random
 import time
 
 counter = 0
+winner = None
 
 def rollDice(side):
     result = random.randint(1, side)
@@ -29,110 +30,122 @@ def strengthGenerator():
     strength = int(((roll12_6 * roll12_8) /2) + 12)
     return strength
 
+
+print("                     === ⚔️⚔️ ", "\033[35m", "CHARACTER BUILDER", "\033[0m", " ⚔️⚔️ ===")
+print()
+time.sleep(0.2)
+print("                              ::GAME CONTROLS:: ")
+print("Create your legend by naming it, choosing its type, and generating its health and strength stats.")
+time.sleep(1)
+print()
+print()
+
+#   Character #1 -- name and type
+username1 = input("Name your legend: \n")
+time.sleep(0.5)
+type1 = character_type()
+print()
+
+#   Character #1 -- health status and strength
+print("\033[1;31m", username1, "of the almightyy!!", "\033[0m")
+time.sleep(1)
+health1 = healthGenerator()
+print("HEALTH: ", health1)
+time.sleep(0.5)
+strength1 = strengthGenerator()
+print("STRENGTH: ", strength1)
+print()
+time.sleep(1)
+
+#Character #2 -- name and type
+print("Who are they battling?")
+username2 = input("Name your legend: \n")
+time.sleep(0.5)
+type2 = character_type()
+print()
+
+#   Character #2 -- health status and strength
+print("\033[1;31m", username2, "of the magnificents!!", "\033[0m")
+time.sleep(1)
+health2 = healthGenerator()
+print("HEALTH: ", health2)
+time.sleep(0.5)
+strength2 = strengthGenerator()
+print("STRENGTH: ", strength2)
+print()
+time.sleep(1)
+print(health2)
+os.system("clear")
+
+
 while True:
-    print("                     === ⚔️⚔️ ", "\033[35m", "CHARACTER BUILDER", "\033[0m", " ⚔️⚔️ ===")
+    print("⚔️ BATTLE TIME ⚔️")
+    print()
+    print("The battle begins!")
+    # battle()
+
+    # to find the difference between the strengths of the characters
+    difference = abs(strength1 - strength2) + 1
+    # 'abs' means ABSOLUTE, to always get a positive number.
+
+    char1 = rollDice(6)
+    char2 = rollDice(6)
+    if char1 == char2:
+        counter += 1
+        print("There is no winner for round", counter, "! Their swords clash and both take minor damage.")
+        print()
+    elif char1 > char2:
+        counter += 1
+        health2 = health2 - difference
+        print(username1, "01 wins round", counter)
+        print()
+    elif char2 > char1:
+        counter += 1
+        health1 = health1 - difference
+        print(username2, "02 owns round", counter)
+        print()
+
+
+    print(username1, "health is now:", health1)
+    print(username2, "health is now:", health2)
     print()
     time.sleep(0.2)
-    print("                              ::GAME CONTROLS:: ")
-    print("Create your legend by naming it, choosing its type, and generating its health and strength stats.")
-    time.sleep(1)
-    print()
-    print()
 
-    #   Character #1 -- name and type
-    username1 = input("Name your legend: \n")
-    time.sleep(0.5)
-    type1 = character_type()
-    print()
-
-    #   Character #1 -- health status and strength
-    print("\033[1;31m", username1, "of the almightyy!!", "\033[0m")
-    time.sleep(1)
-    health1 = healthGenerator()
-    print("HEALTH: ", health1)
-    time.sleep(0.5)
-    strength1 = strengthGenerator()
-    print("STRENGTH: ", strength1)
-    print()
-    time.sleep(1)
-
-    #Character #2
-    print("Who are they battling?")
-    username2 = input("Name your legend: \n")
-    time.sleep(0.5)
-    type2 = character_type()
-    print()
-    
-    #   Character #2 -- health status and strength
-    print("\033[1;31m", username2, "of the magnificents!!", "\033[0m")
-    time.sleep(1)
-    health2 = healthGenerator()
-    print("HEALTH: ", health2)
-    time.sleep(0.5)
-    strength2 = strengthGenerator()
-    print("STRENGTH: ", strength2)
-    print()
-    time.sleep(1)
-    print(health2)
-    os.system("clear")
-    
-
-    while True:
-        print("⚔️ BATTLE TIME ⚔️")
-        print()
-        print("The battle begins!")
-        # battle()
-    
-        char1 = rollDice(6)
-        char2 = rollDice(6)
-        if char1 == char2:
-            print("It's a tie!")
-            print("There is no winner!")
-            counter = +1
-            print()
-        elif char1 > char2:
-            print("Character 1 wins!")
-            print(username1, "wins this round!")
-            counter = +1
-            print()
-        elif char2 > char1:
-            print("Character 2 wins!")
-            print(username2, "owns this round!")
-            counter = +1
-            print()
-    
-        strength1 = int(strength1 - char1)
-        strength2 = int(strength2 - char2)
-    
-        # losers strength reduction
-        characterOneWins = (strength1 - strength2) +1
-        strength2 = strength2 - characterOneWins
-        characterTwoWins = (strength2 - strength1) +1
-        strength1 = strength1 - characterTwoWins
-        #char1 = username1
-        #char2 = username2
-      
-        if counter == 3:
-            break
-        elif strength1 <= 0:
-            print(username2, "the chief among", type1, "IS THE UNDISPUTED CHAMPION!")
-            break
-        elif strength2 <= 0:
-            print(username1, "the chief among", type2, "IS THE UNDISPUTED CHAMPION!")
-        else:
-            continue
-    
-    #   Game controls
-    userRequest = input("Press 1 to play again or press 2 to exit the game >> ")
-    if userRequest == "1":
-        time.sleep(1)
-        os.system("clear")
-        continue
-    elif userRequest == "2":
-        time.sleep(1)
+    if health1 <= 0:
+        print(username1, "has died!")
+        print(username2, "the chief among", type2, "IS THE UNDISPUTED CHAMPION!")
+        winner = username2
+        break
+    elif health2 <= 0:
+        print(username2, "has died!")
+        print(username1, "the chief among", type1, "IS THE UNDISPUTED CHAMPION!")
+        winner = username1
         break
     else:
-        print("CHOOSE A VALID OPTION NEXT TIME !!!")
+        print("The battle rages ooonnnn!!! They're bothe standing for the next round.")
+        print()
+        print()
+        continue
+
+time.sleep(1)
+os.system("clear")
+print()
+print("⚔️ BATTLE TIME ⚔️")
+print()
+print("🏆🏆🏆 THE WINNER:", "\033[33m", winner, "🏆🏆🏆", "\033[0m", "won after", counter, "rounds.")
+
+
+'''#   Game controls
+userRequest = input("Press 1 to play again or press 2 to exit the game >> ")
+if userRequest == "1":
+    time.sleep(1)
+    os.system("clear")
+    continue
+elif userRequest == "2":
+    time.sleep(1)
+    break
+else:
+    print("CHOOSE A VALID OPTION NEXT TIME !!!")'''
 
 
 print()
